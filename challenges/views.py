850d065c735e565d31c10d9cb5bde572c0207557
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2022-01-19 22:57:07 +0100

    views.py has been upgraded.

--- challenges/views.py
+++ challenges/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def january(reqest):
-    return HttpResponse("Eat only meat for the entire month")
+from django.http import HttpResponse, HttpResponseNotFound
 
 
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes every day")
+monthly_challenges = {
+    "january": "Eat only meat for the entire month",
+    "february": "Walk for at least 20 minutes every day",
+    "march": "Learn Django for at least 20 minutes every day",
+    "april": "Play with your kids for at least 1 hour every day."
+}
 
 
 def monthly_challenge(request, month: str):
-    return HttpResponse(f"Challenge for {month}. month is a {type(month).__name__}")
+    if month not in monthly_challenges:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponse(monthly_challenges[month])
 
 
 def monthly_challenge_by_number(request, month: int):
-    print(type(month))
-    return HttpResponse(f"Challenge for {month}. month is an {type(month).__name__}")
+    if month > len(monthly_challenges) or month < 1:
+        return HttpResponseNotFound("This month is not supported!")
+
+    month: str = list(monthly_challenges.keys())[month - 1]
+    return HttpResponse(monthly_challenges[month])
