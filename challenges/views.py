b794eb55ece414ef75e6a815891d3fcf8655a5e3
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2022-01-19 22:33:30 +0100

    Path converters have been introduced.

--- challenges/views.py
+++ challenges/views.py
@@ -11,5 +11,10 @@ def february(request):
     return HttpResponse("Walk for at least 20 minutes every day")
 
 
-def monthly_challenge(request, month):
-    return HttpResponse(f"Challenge for {month}")
+def monthly_challenge(request, month: str):
+    return HttpResponse(f"Challenge for {month}. month is a {type(month).__name__}")
+
+
+def monthly_challenge_by_number(request, month: int):
+    print(type(month))
+    return HttpResponse(f"Challenge for {month}. month is an {type(month).__name__}")
