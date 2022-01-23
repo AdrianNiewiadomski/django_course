f1193019a1ed08f6585048954846f20f38e36773
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2022-01-23 21:37:29 +0100

    The main page of challenges has been added. The reverse function and the named URLs have been used to dynamically redirect requests.

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge)
-    ]
+    path("", views.index),  # challenges/
+    path("<int:month>", views.monthly_challenge_by_number),  # challenges/1
+    path("<str:month>", views.monthly_challenge, name="month-challenge")  # challenges/january
+]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 
 monthly_challenges = {
@@ -8,6 +9,16 @@ monthly_challenges = {
     "march": "Learn Django for at least 20 minutes every day",
     "april": "Play with your kids for at least 1 hour every day."
 }
+
+
+def index(request):
+    response_text = "<ul>"
+    for month in monthly_challenges.keys():
+        redirect_path = reverse("month-challenge", args=[month])
+        response_text += f"<li><a href=\"{redirect_path}\">{month}</a></li>"
+    response_text += "</ul>"
+
+    return HttpResponse(response_text)
 
 
 def monthly_challenge(request, month: str):
@@ -23,4 +34,5 @@ def monthly_challenge_by_number(request, month: int):
 
     months = list(monthly_challenges.keys())
     redirect_month: str = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
